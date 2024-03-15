706d5cffd69717d842cf741980213aab69752bc9
Luis Rojas <lrojas@snabb-it.cl>
2024-03-15 16:55:18 -0300

    Correccion Signals de control de calidad por humedad de lote

--- controlcalidad/signals.py
+++ controlcalidad/signals.py
@@ -6,8 +6,11 @@ from django.dispatch import receiver
 @receiver(post_save, sender=CCRecepcionMateriaPrima)
 def comprueba_si_aprueba_cc_por_humedad_recepcionmp(sender, instance, created, **kwargs):
     print(instance.humedad)
-    if instance.humedad <= 6:
-        CCRecepcionMateriaPrima.objects.filter(pk=instance.pk).update(estado_cc='1')
+    if instance and created:
+        pass
     else:
-        CCRecepcionMateriaPrima.objects.filter(pk=instance.pk).update(estado_cc='0')
+        if instance.humedad <= 6:
+            CCRecepcionMateriaPrima.objects.filter(pk=instance.pk).update(estado_cc='1')
+        else:
+            CCRecepcionMateriaPrima.objects.filter(pk=instance.pk).update(estado_cc='0')
     #     
