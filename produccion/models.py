716622ff4d4759301ee226359a78d14eba7dcd40
Luis Rojas <lrojas@snabb-it.cl>
2024-04-03 20:31:55 -0300

    Incluyo bins de Resultantes de Reproceso para Programa de Reproceso (loop de sistema)

--- produccion/models.py
+++ produccion/models.py
@@ -120,7 +120,7 @@ class OperariosEnReproceso(ModeloBase):
     
 class BinsEnReproceso(ModeloBaseHistorico):
     reproceso           = models.ForeignKey(Reproceso, on_delete=models.CASCADE)
-    limite_opciones     = models.Q(app_name = 'bodegas', model = 'bodegag1') | models.Q(app_name = 'bodegas', model = 'bodegag2')
+    limite_opciones     = models.Q(app_name = 'bodegas', model = 'bodegag1') | models.Q(app_name = 'bodegas', model = 'bodegag2') | models.Q(app_name = 'bodegas', model = 'bodegag1reproceso') | models.Q(app_name = 'bodegas', model = 'bodegag2reproceso')
     tipo_bin_bodega     = models.ForeignKey('contenttypes.ContentType', on_delete=models.SET_NULL, null=True, blank=True, limit_choices_to=limite_opciones)
     id_bin_bodega       = models.PositiveIntegerField(blank=True, null=True)
     bin_bodega          = GenericForeignKey('tipo_bin_bodega', 'id_bin_bodega')
--- produccion/signals.py
+++ produccion/signals.py
@@ -27,3 +27,24 @@ def vincula_resultante_bodega_rs_g1_g2(sender, created, instance, **kwargs):
             codigo = str('G1-')+random_codigo_tarja()
             instance.codigo_tarja = codigo
         instance.save()
+        
+        
+@receiver(post_save, sender=TarjaResultanteReproceso)
+def vincula_resultante_reproceso_bodega_rs_g1_g2(sender, created, instance, **kwargs):
+    def random_codigo_tarja(lenght=6):
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(lenght))
+    
+    if created and instance:
+        if instance.tipo_resultante == '3':
+            BodegaG2Reproceso.objects.update_or_create(produccion=instance, kilos_fruta=instance.peso)
+            codigo = str('G2-')+random_codigo_tarja()
+            instance.codigo_tarja = codigo
+        elif instance.tipo_resultante == '2':
+            BodegaResiduos.objects.update_or_create(produccion=instance, kilos_residuo=instance.peso)
+            codigo = str('RS-')+random_codigo_tarja()
+            instance.codigo_tarja = codigo
+        elif instance.tipo_resultante == '1':
+            BodegaG1Reproceso.objects.update_or_create(produccion=instance, kilos_fruta=instance.peso)
+            codigo = str('G1-')+random_codigo_tarja()
+            instance.codigo_tarja = codigo
+        instance.save()
